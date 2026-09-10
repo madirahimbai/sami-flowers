ae42d020593f7b1e03fc125aac8179c82ebe9ce2
madirahimbai <madi_rahimbai@mail.ru>
2026-09-10 20:40:40 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: '*.public.blob.vercel-storage.com' }
-    ]
-  }
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
